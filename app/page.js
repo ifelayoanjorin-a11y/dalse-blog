910181692b50a70e80b9ce339ed9cb1413681500
Ifelayo Anjorin <ifelayoanjorin-a11y>
2026-08-07 11:24:41 +0100

    implementing Navbarr and stilling

--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Home page</p>;
+  return (
+    <>
+      <section className="bg-primary px-5 py-10 md:py-16 text-center">
+        <div className="max-w-3xl px-6 py-4 inline-block bg-white dark:bg-black">
+          <h1>Welcome To Dalse Blog</h1>
+        </div>
+        <p className="text-white text-sm mt-4 max-w-xl mx-auto dark:text-black">
+          Read post from community or share your own story
+        </p>
+        <form>
+          <div>
+            <input type="text" placeholder="Search Post.." />
+            <button>Button</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,13 +5,23 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <header className="bg-gray-500 p-6">
-        <nav>
-          <Link href="/"></Link>
-          <span>Dalse Blog</span>
-          <div>
-            <Link href="/post/create">Create</Link>
-            <Link href="/profile">My Post</Link>
+      <header className="bg-white shadow-sm py-3 dark:text-black">
+        <nav className="flex items-center max-w-7xl mx-auto justify-between">
+          <Link href="/">
+            <span className="font-bold text-xl text-blue-500">Dalse</span>
+            <span className="text-primary font-bold dark:text-black">Blog</span>
+          </Link>
+
+          <div className="flex item-center gap-5 text-sm">
+            <Link
+              href="/post/create"
+              className="font-medium hover:text-blue-500"
+            >
+              Create
+            </Link>
+            <Link href="/profile" className="font-medium hover:text-blue-500">
+              My Post
+            </Link>
             <ModeToggle />
           </div>
         </nav>
